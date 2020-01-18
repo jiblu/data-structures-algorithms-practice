f1c41c65e7695e3beed7651fafb0d6e82e0def3f
ji <jkim0107@gmail.com>
2020-01-18 11:09:34 -0800

    need to review multiple pointer

---
+++ multiplePointers-sumZero.js
@@ -0,0 +1,16 @@
+/*
+Write a function called sumZero which accepts a sorted array of integers. The function should find the first pair where the sum is 0. Return an array that includes both values that zum to zero or undefined if a pair does not exist.
+
+sumZero([-3,-2,-1,0,1,2,3]) [-3,3]
+sumZero([-4,-3,-2,0,1,2]) undefined
+sumZero([-2,0,1,3]) undefined
+sumZero([1,2,3]) undefined
+*/
+
+//https://www.udemy.com/course/js-algorithms-and-data-structures-masterclass/learn/lecture/11183942#overview
+
+//try again next time: could not get it 1/18
+
+const sumZero = (sortedArray) => {
+  
+}
---
+++ multiplePointers.js
@@ -0,0 +1,14 @@
+/*
+creating pointers or values that correspond to an index or position and move towards the beginning, end or middle based on a certain condition
+
+very efficient for solving problems with minimal space complexity as well
+
+
+
+[-4, -3, -2, -1, 0, 1, 2, 5]
+"alksjdalksjdlkasjdlks"
+
+usually you have a linear structure like array or string
+you have two pointers (one at end and one at beginning)
+and they move towards the middle
+*/
